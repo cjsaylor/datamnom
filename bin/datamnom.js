f766f084b5457f587f3e9c0cea7984ba5965675d
Stephen Young <young.steveo@gmail.com>
2015-06-06 14:28:27 -0400

    datamnom is now partially using a json config

--- bin/datamnom.js
+++ bin/datamnom.js
@@ -2,60 +2,41 @@ var path = require('path');
 var fs = require('fs');
 var client = require('../src/client');
 var FixedLengthIngestor = require('../src/ingesters/fixed-length-text');
+var config = require(path.normalize(__dirname + '/../inbound/FY2013.json'));
+var destination = new FixedLengthIngestor();
+var _ = require('lodash');
+var esConfig = config.destinations.elasticsearch;
 
-var inbound = fs.createReadStream(path.normalize(__dirname + '/../inbound/FY2013.txt'));
+destination.on('error', console.error);
 
-var typeMap = {
-	vendorTransaction: {
-		properties: {
-			year: {type: 'integer'},
-			account: {type: 'string'},
-			name: {type: 'string', index: 'not_analyzed'},
-			address1: {type: 'string'},
-			address2: {type: 'string'},
-			address3: {type: 'string'},
-			city: {type: 'string', index: 'not_analyzed'},
-			state: {type: 'string', index: 'not_analyzed'},
-			zip: {type: 'string', index: 'not_analyzed'},
-			payment_account: {type: 'integer'},
-			pay_agency: {type: 'string', index: 'not_analyzed'},
-			state_document_no: {type: 'string', index: 'not_analyzed'},
-			payment_no: {type: 'string', index: 'not_analyzed'},
-			date: {type: 'date'},
-			agency_document_no: {type: 'string', index: 'not_analyzed'},
-			contact_phone: {type: 'string', index: 'not_analyzed'},
-			object_code: {type: 'string', index: 'not_analyzed'},
-			object_description: {type: 'string', index: 'not_analyzed'},
-			amount: {type: 'float'}
-		}
-	}
-}
 
-var outbound = new FixedLengthIngestor();
+_.forEach(config.sources, function(ingestOptions, filename) {
+	var source = fs.createReadStream(path.normalize(__dirname + '/../inbound/' + filename));
 
-outbound.on('error', console.error);
-
-client.indices.get({index: 'flvendors'})
-	.catch(function() {
-		return client.indices.create({index: 'flvendors'});
-	})
-	.then(function() {
-		return client.indices.getMapping({index: 'flvendors', type: 'vendorTransaction'});
-	})
-	.then(function(mapping) {
-		if (!Object.keys(mapping).length) {
-			throw new Error('No mapping.');
-		}
-	})
-	.catch(function() {
-		return client.indices.putMapping({
-			index: 'flvendors',
-			type: 'vendorTransaction',
-			body: typeMap
-		});
-	})
-	.then(function() {
-		inbound.pipe(outbound);
-	})
-	.catch(console.error)
-	.done();
+	client.indices.get({index: esConfig.index})
+		.catch(function() {
+			return client.indices.create({index: esConfig.index});
+		})
+		.then(function() {
+			// @todo type is still hardcoded
+			return client.indices.getMapping({index: esConfig.index, type: 'vendorTransaction'});
+		})
+		.then(function(mapping) {
+			if (!Object.keys(mapping).length) {
+				throw new Error('No mapping.');
+			}
+		})
+		.catch(function() {
+			return client.indices.putMapping({
+				index: esConfig.index,
+				// @todo type is still hardcoded
+				type: 'vendorTransaction',
+				body: esConfig.types
+			});
+		})
+		.then(function() {
+			source.pipe(destination);
+		})
+		.catch(console.error)
+		.done();
+});
