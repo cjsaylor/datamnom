790f5be8f35a6cd8f4c709a99cfab40a6327bd37
Chris Saylor <cjsaylor@gmail.com>
2019-05-25 16:34:37 -0400

    Abstract file importer that handles getting readable streams.

---
+++ src/fileImporter.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const { Writable } = require('stream')
+const path = require('path');
+const fs = require('fs');
+
+const junk = require('junk');
+
+const globCB = require('glob')
+const glob = function (path) {
+	return new Promise((res, rej) => {
+		globCB(path, (err, matches) => {
+			if (err) {
+				rej(err)
+				return
+			}
+			res(matches)
+		})
+	})
+}
+
+class FileImporter extends Writable {
+
+	static async getStreams(globTemplate, esConfig, fileOptions) {
+		const files = await glob(globTemplate)
+		return files.filter(junk.not).map(file => {
+			const { size } = fs.statSync(file)
+			const inputStream = fs.createReadStream(file)
+			const outputStream = new this(esConfig, fileOptions)
+			const title = path.basename(file, path.extname(file))
+			return {inputStream, outputStream, size, title}
+		})
+	}
+}
+
+module.exports = FileImporter
