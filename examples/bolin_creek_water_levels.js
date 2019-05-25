f606411291ee24f9c395254354d9c6b52f1023a1
Chris Saylor <cjsaylor@gmail.com>
2019-05-25 07:20:53 -0400

    Added bolin creek water levels example.

---
+++ examples/bolin_creek_water_levels.js
@@ -0,0 +1,47 @@
+'use strict'
+
+// @see https://waterdata.usgs.gov/nwis/uv?cb_00060=on&cb_00065=on&format=html&site_no=0209734440
+
+module.exports = {
+	"ingestor": {
+		"class": "csv",
+		"delimiter": "\t",
+		"headers": [
+			"Agency Code",
+			"Site Number",
+			"Timestamp",
+			"Timezone",
+			"Gauge Height (ft)",
+			"GH Approval",
+			"Discharge (ft/s)",
+			"DC Approval"
+		],
+		"mapper": function (record) {
+			record['Timestamp'] = new Date(`${record['Timestamp']} ${record['Timezone']}`)
+			;[
+				'Gauge Height (ft)',
+				'Discharge (ft/s)'
+			].forEach(key => record[key] = parseFloat(record[key]))
+			delete record['Timezone']
+			return record
+		}
+	},
+	"destinations": {
+		"elasticsearch": {
+			"index": "bolin_creek_water_levels",
+			"types": {
+				"properties": {
+					"Gauge Height (ft)": {
+						"type": "float"
+					},
+					"Discharge (ft/s)": {
+						"type": "float"
+					},
+					"Timestamp": {
+						"type": "date"
+					}
+				}
+			}
+		}
+	}
+}
