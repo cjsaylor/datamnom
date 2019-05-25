46114db5cb34455741f7891d24a8fe06dce990fa
Chris Saylor <cjsaylor@gmail.com>
2019-05-24 22:23:11 -0400

    Added a sample import configuration for chapel hill air quality data.

---
+++ examples/air_quality.js
@@ -0,0 +1,64 @@
+'use strict'
+
+// @see https://www.chapelhillopendata.org/explore/dataset/local-air-quality/information/
+
+module.exports = {
+	"ingestor": {
+		"class": "csv",
+		"delimiter": ";",
+		"headers": [
+			"Last Check",
+			"Current Particulate Matter 2.5 Value (PM 2.5)",
+			"PM 2.5 (10 Minute Avg.)",
+			"PM 2.5 (30 Minute Avg.)",
+			"PM 2.5 (1 Hour Avg.)",
+			"PM 2.5 (6 Hour Avg.)",
+			"PM 2.5 (24 Hour Avg.)",
+			"PM 2.5 (One Week Avg.)",
+			"Temp (F)",
+			"Humidity (%)",
+			"Pressure (mbar)",
+			"Site Label",
+			"Inside/Outside",
+			"Latitude",
+			"Longitude",
+			"Uptime (Seconds)",
+			"RSSI (WiFi signal strength dBm)",
+			"Hardware Issues",
+			"Age of Data at Check (minutes)",
+			"Geo Point (0)"
+		],
+		"mapper": function (record) {
+			record['Geo Point'] = record['Geo Point (0)']
+			;['Latitude', 'Longitude', 'Geo Point (0)'].forEach(key => delete record[key])
+			;[
+				"PM 2.5 (10 Minute Avg.)",
+				"PM 2.5 (30 Minute Avg.)",
+				"PM 2.5 (1 Hour Avg.)",
+				"PM 2.5 (6 Hour Avg.)",
+				"PM 2.5 (24 Hour Avg.)",
+				"PM 2.5 (One Week Avg.)",
+				'Pressure (mbar)'
+			].forEach(key => {
+				record[key] = parseFloat(record[key])
+			})
+			;[
+				'Temp (F)',
+				'Humidity (%)'
+			].forEach(key => record[key] = parseInt(record[key], 10))
+			return record
+		}
+	},
+	"destinations": {
+		"elasticsearch": {
+			"index": "air_quality",
+			"types": {
+				"properties": {
+					"Geo Point": {
+						"type": "geo_point"
+					}
+				}
+			}
+		}
+	}
+}
