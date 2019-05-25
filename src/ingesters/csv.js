44441009a45ad5e0b01d4a72361a5069b49da74b
Chris Saylor <cjsaylor@gmail.com>
2019-05-25 07:20:35 -0400

    Buffer csv until at least 1000 bulk commands to ES.

--- src/ingesters/csv.js
+++ src/ingesters/csv.js
@@ -11,30 +11,36 @@ class CSVIngestor extends Writable {
 		super()
 		this.fileOptions = fileOptions
 		this.esConfig = esConfig
+		this.buffer = ''
 		this.noopMap = record => record
 	}
 
 	async _write(chunk, encoding, next) {
 		next = typeof encoding === 'function' ? encoding : next
-		const buffer = chunk.toString()
-		const records = await csvParse(buffer, {
-			headers: this.fileOptions.headers,
-			separator: this.fileOptions.delimiter,
-		})
-		const operations = records
-			.map(this.fileOptions.mapper || this.noopMap)
-			.reduce((prev, cur) => ([
-				...prev,
-				{
-					index: {
-						_index: this.esConfig.index,
-						_id: crypto.createHash('md5').update(JSON.stringify(cur)).digest("hex"),
-					}
-				},
-				cur,
-			]), [])
+		this.buffer += chunk.toString()
+		if (this.buffer.split("\n").length < 1000) {
+			next()
+			return
+		}
 		try {
+			const records = await csvParse(this.buffer, {
+				headers: this.fileOptions.headers,
+				separator: this.fileOptions.delimiter,
+			})
+			const operations = records
+				.map(this.fileOptions.mapper || this.noopMap)
+				.reduce((prev, cur) => ([
+					...prev,
+					{
+						index: {
+							_index: this.esConfig.index,
+							_id: crypto.createHash('md5').update(JSON.stringify(cur)).digest("hex"),
+						}
+					},
+					cur,
+				]), [])
 			await client.bulk({body: operations})
+			this.buffer = ''
 			next()
 		} catch (e) {
 			console.trace(e)
